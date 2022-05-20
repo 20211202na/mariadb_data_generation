d6c48b8d0b359311269ec89f513db5ae650ff327
20211202na <96566054+20211202na@users.noreply.github.com>
2022-05-20 15:22:57 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+server=$1
+run_num=$2
+
+for ((j=0;j<$run_num;j++));do
+{
+    python maria.py -f ${j}
+    wait
+    python init.py $server
+}
+done
+
+python3 group_data.py $run_num
+wait
+python3 check_rr.py $run_num
